14e79d9c689fe2120e7f90025cf4105e1bf58518
Dejan Milosevic <milosevic.d@gmail.com>
2025-02-09 22:22:57 +0000

    fixed eslint

---
+++ src/app/api/wamsg/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+    return NextResponse.json({ message: "Hello World" });
+}
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    console.log(body);
+    return NextResponse.json({ message: "OK" });
+}
+
